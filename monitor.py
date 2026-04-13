8e23d92ba2d70ae96376e5293028e940ca05aeaa
GalaxieNolla <111712655+GalaxieNolla@users.noreply.github.com>
2026-04-13 14:09:05 -0700

    Update monitor.py to detect active window

--- monitor.py
+++ monitor.py
@@ -1,15 +1,13 @@
 import time
 from pynput import mouse, keyboard
 import threading
+from AppKit import NSWorkspace
 
 class ActivityMonitor:
     def __init__(self):
         self.last_activity = time.time()
-        
-        # Start the listeners in background threads
         self.mouse_listener = mouse.Listener(on_move=self.update_activity, on_click=self.update_activity)
         self.key_listener = keyboard.Listener(on_press=self.update_activity)
-        
         self.mouse_listener.start()
         self.key_listener.start()
 
@@ -18,3 +16,8 @@ class ActivityMonitor:
 
     def get_idle_time(self):
         return time.time() - self.last_activity
+
+    def get_active_app(self):
+        # This talks to macOS to find the frontmost app
+        active_app = NSWorkspace.sharedWorkspace().frontmostApplication()
+        return active_app.localizedName()
