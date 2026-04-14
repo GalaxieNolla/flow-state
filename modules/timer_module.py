a8d9d31b6b3ef9d37c55715d63e869baa281948c
GalaxieNolla <111712655+GalaxieNolla@users.noreply.github.com>
2026-04-13 19:05:32 -0700

    Update timer_module.py w styles

--- modules/timer_module.py
+++ modules/timer_module.py
@@ -1,11 +1,12 @@
 import tkinter as tk
+from visuals import styles
 
 class StudyTimer:
     def __init__(self, root, status_label):
         self.root = root
         self.status_label = status_label
         self.seconds_left = 0
-        self.running = False # Used to stop app.py from flashing
+        self.running = False # no more flashing
 
     def start(self, study_mins=25, break_mins=5):
         self.running = True
@@ -17,15 +18,15 @@ class StudyTimer:
         if self.seconds_left >= 0:
             mins, secs = divmod(self.seconds_left, 60)
             # Big, clean timer font
-            self.status_label.config(text=f"{mins:02d}:{secs:02d}", font=("Helvetica", 40, "bold"))
+            self.status_label.config(text=f"{mins:02d}:{secs:02d}", font=styles.FONT_DISPLAY)
             self.seconds_left -= 1
             self.root.after(1000, lambda: self.tick(break_mins))
         else:
             if not self.is_break:
-                self.status_label.config(text="BREAK TIME", fg="#00ff73", font=("Helvetica", 20))
+                self.status_label.config(text="BREAK TIME", fg=styles.CYAN_FOG, font=styles.FONT_STATUS)
                 self.seconds_left = break_mins * 60
                 self.is_break = True
                 self.root.after(3000, lambda: self.tick(break_mins))
             else:
-                self.status_label.config(text="FOCUS", fg="#40c9c9")
-                self.running = False # Allow app.py to take over again
+                self.status_label.config(text="FOCUS", fg=styles.PURPLE_GLOW)
+                self.running = False # app.py time
