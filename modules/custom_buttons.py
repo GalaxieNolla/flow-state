c62e07e7c6ee4403ff72609611a5459aade24ae7
GalaxieNolla <111712655+GalaxieNolla@users.noreply.github.com>
2026-04-14 00:36:57 -0700

    Update custom_buttons.py sizw

--- modules/custom_buttons.py
+++ modules/custom_buttons.py
@@ -5,7 +5,7 @@ from visuals import styles
 
 def create_mode_button(canvas, x, y, text, command):
     visuals_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "visuals")
-    size = (150, 90)
+    size = (220, 110)
     
     active_path = os.path.join(visuals_dir, "active.png")
     inactive_path = os.path.join(visuals_dir, "inactive.png")
