71f087dd032b567c3fd2d880f73f4501ba0452f2
GalaxieNolla <111712655+GalaxieNolla@users.noreply.github.com>
2026-04-15 22:49:52 -0700

    Update leaderboard.py

--- modules/leaderboard.py
+++ modules/leaderboard.py
@@ -88,7 +88,7 @@ class Leaderboard:
             self.canvas.create_text(col_x[i], start_y,
                 text=col, font=("Cinzel", 14), fill=styles.JINX_BLUE)
 
-        self.canvas.create_line(cx - 180, start_y + 20, cx + 180, start_y + 50,
+        self.canvas.create_line(cx - 180, start_y + 20, cx + 180, start_y + 20,
             fill=styles.JINX_DIVIDER, width=1)
 
         if not sessions:
