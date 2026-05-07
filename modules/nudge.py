d412ebdcd45a59e5c79e54272e74c446b7aeb30f
GalaxieNolla <111712655+GalaxieNolla@users.noreply.github.com>
2026-05-07 15:23:49 -0700

    normal enter

--- modules/nudge.py
+++ modules/nudge.py
@@ -26,13 +26,19 @@ class Nudge:
 
     # Messages to randomly choose from :D
     STRESSED_MESSAGES = [
-        "Ekko: 'Sometimes taking a leap forward\rmeans leaving a few things behind.'"
+        "Ekko: 'Sometimes taking a leap forward
+        means leaving a few things behind.'"
         "Vi: 'You're stronger than you think.'"
-        "Viktor: 'When you're going to change\rthe world, don't ask for permission.'"
-        "Vi: 'We've all had bad days.\rBut we learn, and we stick together.'"
-        "Vander: 'You've got a good heart.\rDon't ever lose it."
-        "Ekko: 'In case I don't remember to tell you\rtomorrow, you've always meant the world to me.'"
-        "Jayce: 'There is beauty in imperfections.\rThey made you who you are.'"
+        "Viktor: 'When you're going to change
+        the world, don't ask for permission.'"
+        "Vi: 'We've all had bad days.
+        But we learn, and we stick together.'"
+        "Vander: 'You've got a good heart.
+        Don't ever lose it."
+        "Ekko: 'In case I don't remember to tell you
+        tomorrow, you've always meant the world to me.'"
+        "Jayce: 'There is beauty in imperfections.
+        They made you who you are.'"
         "Vi: 'Be honest. Be patient.'"
         "you're doing better than you think",
         "one thing at a time. breathe.",
@@ -47,7 +53,8 @@ class Nudge:
         "look out a window for 20 seconds 👀",
         "roll your shoulders back. unclench your jaw.",
         "step outside for 5 mins if you can ☀️",
-        "Vander: 'You need to fill your own cup\revery now and again.'"
+        "Vander: 'You need to fill your own cup
+        every now and again.'"
         "Vi: 'You're an all-right shot.'"
         "Jinx: 'Big fat hero ❤︎'"
     ]
